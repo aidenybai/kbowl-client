32772b6f24769c966407e170b75f9ed6e2d38ffc
Aiden Bai <aiden.bai05@gmail.com>
2022-02-17 16:28:39 -0800

    Fix legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
